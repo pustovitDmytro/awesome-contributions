a2290d5abc1080331afdea957e68f1153dc7206a
pustovitDmytro <pustic@gmail.com>
2018-01-24 22:49:31 +0200

    1 color mapping

--- gen.py
+++ gen.py
@@ -7,6 +7,8 @@ from datetime import date, timedelta
 from subprocess import call
 
 imgDir = 'images/'
+scriptDir = 'scripts/'
+gitfile = '.blob'
 # GitHub uses Sunday-starting weeks, so add 1
 offset = (date.today().weekday() + 1) % 7
 rows = 7
@@ -24,6 +26,20 @@ def commit(days_ago, msg):
               t + '"' + ' git commit -m "' + msg + '" 2>&1 >/dev/null')
 
 
+def write_px(x, y, intensity, prefix=""):
+    days_ago = numdays + offset - (x * rows + y)
+    # d = date.today() - timedelta(days=days_ago)
+    # t = str(d) + " 00:00:00"
+    # print "val=",intensity, "x",x,"y",y,"date:",d
+    print(x, y)
+    print(intensity)
+    # print(days_ago)
+    print('-------------')
+    # for i in range(0, intensity):
+    # msg = prefix + os.urandom(8).encode("hex")
+    # commit(days_ago, msg)
+
+
 def rgb2gray(rgb):
     r, g, b = rgb[0:3]  # ignore alpha for now
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
@@ -31,30 +47,29 @@ def rgb2gray(rgb):
     return gray
 
 
-def write_px(x, y, intensity, prefix=""):
-    days_ago = numdays + offset - (x * rows + y)
-    d = date.today() - timedelta(days=days_ago)
-    t = str(d) + " 00:00:00"
-    # print "val=",intensity, "x",x,"y",y,"date:",d
-    for i in range(0, intensity):
-        msg = prefix + os.urandom(8).encode("hex")
-        commit(days_ago, msg)
-
-# Use this function to process a 52x7 image as grayscale
-
-
 def process_image(path):
     img = Image.open(path)
+    imap = path + '.map'
+    file = open(imap, "w")
     px = img.load()
     size = img.size
     if (52, 7) != size:
         raise Exception("Image should be 52x7, got " + size)
+    for y in range(size[1]):
+        file.write('\n')
+        for x in range(size[0]):
+            val = 255 - int(rgb2gray(px[x, y]))
+            val //= 16
+            if(val>0):
+                file.write("#")
+            else: file.write(' ')
     for x in range(size[0]):
-        # print "processed line",x
         for y in range(size[1]):
             val = 255 - int(rgb2gray(px[x, y]))
-            val /= 16
-            write_px(x, y, val, prefix="ign-")
+            val //= 16
+            print(val)
+            # write_px(x, y, val, prefix="ign-")
+    file.close()
 
 
 def process_text(txt, offset=2):
