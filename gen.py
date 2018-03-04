cb2784efb557950437d21aefff31f2a03b44e723
pustovitDmytro <pustic@gmail.com>
2018-03-04 14:45:06 +0200

    extra space

--- gen.py
+++ gen.py
@@ -19,10 +19,10 @@ def commit(file, stamp, msg, author='pustovitDmytro <dipustovit@gmail.com>'):
     # file.write("\nsleep 1")
     file.write("\necho '" + stamp + '#' + msg + "' >> " + gitfile)
     # file.write("\nsleep 1")
-    file.write('\ngit commit -a -m "' + msg + '" --date ' + stamp + '--author="' + author + '"')
+    file.write('\ngit commit -a -m "' + msg + '" --date ' + stamp + ' --author="' + author + '"')
 
 
-def write_px(file, x, y, intensity, prefix="", author):
+def write_px(file, x, y, intensity, prefix="", author=''):
     days_ago = numdays + offset - (x * rows + y)
     for i in range(0, intensity):
         d = datetime.today() - timedelta(days=days_ago, seconds=i)
@@ -38,13 +38,13 @@ def rgb2gray(rgb):
     return gray
 
 
-def process_image(path, count = 20, saveAs='', author):
+def process_image(path, count = 20, saveAs='', author=''):
     img = Image.open(path)
     imap = path + '.map'
     imgName = path.split('/')[-1].split('.')[0]
     file = open(imap, "w")
     savePath = saveAs if saveAs else 'scripts/' + imgName + '.git.sh'
-    fileSh = open(saveAs, "w")
+    fileSh = open(savePath, "w")
     fileSh.write('#!/bin/sh\nset -e\n')
     fileSh.write('git fsck --unreachable\n')
     fileSh.write('git reflog expire --expire=0 --all\n')
@@ -69,7 +69,7 @@ def process_image(path, count = 20, saveAs='', author):
             val = 255 - int(rgb2gray(px[x, y]))
             val //= 255
             val *= count
-            write_px(fileSh, x, y, val, prefix="ign-", author)
+            write_px(fileSh, x, y, val, prefix="ign-", author=author)
     file.close()
     fileSh.close()
 
